c9c876ca1d35795da6b00432e1175d9ab6e72bd4
Victor Oliveira <eu@victoroliveira.com.br>
2022-03-20 21:27:05 -0300

    folder structure updated

--- src/shared/helpers/jwt.ts
+++
@@ -1,9 +0,0 @@
-export function headerFromJwt(jwt: string) {
-  const [header] = jwt.split('.');
-  return JSON.parse(window.atob(header));
-}
-
-export function userFromJwt(jwt: string) {
-  const [, payload] = jwt.split('.');
-  return JSON.parse(window.atob(payload));
-}
