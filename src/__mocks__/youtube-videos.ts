51dc82528676275f258d224abbb1ba363bd61744
Victor Oliveira <eu@victoroliveira.com.br>
2022-03-22 11:49:04 -0300

    feat(mocks): youtube videos response

---
+++ src/__mocks__/youtube-videos.ts
@@ -0,0 +1,260 @@
+import type { YoutubeApi } from 'api/types';
+import { YoutubeVideo } from 'api/videos';
+
+export const data: YoutubeApi<YoutubeVideo> = {
+  kind: 'youtube#videoListResponse',
+  etag: 'jIx4wIFiFM_leyQzRKz0xO2hDKs',
+  items: [
+    {
+      id: 'qF1DTK4U1AM',
+      snippet: {
+        publishedAt: '2022-03-20T18:02:19Z',
+        channelId: 'UCGwu0nbY2wSkW8N-cghnLpA',
+        title: 'Being Not Straight',
+        description:
+          "♥ The Team ♥\nColleen: https://twitter.com/SolarCitrus\nClaudia: https://twitter.com/HiyFi_\nDavidBaronArt: https://twitter.com/DavidBaronArt\nDee Helm: https://twitter.com/DeeRedHelm\nDenny: https://twitter.com/90PercentDenny\nDevon: https://www.youtube.com/channel/UCZaIIdSgN8aEUh7g_BgFobQ\nDuckdee: https://twitter.com/duckdeeArt\nJaystarz: https://www.youtube.com/xjaystarzx\nJRF\nLeslie V\nMotiny: https://www.youtube.com/channel/UCHimPYk1csmmOSGKF4pKelg\nPivots: https://www.youtube.com/user/ThePivotsXXD\nTiffanySeng: https://twitter.com/tiffanyseng_art\n------------------------------------------------------------------------------------\nSUBSCRIBE for more animations!\nhttps://www.youtube.com/c/jaiden\n\nMERCH:\nhttps://jaidenanimations.com/\n\nTWITTER:\nhttps://twitter.com/JaidenAnimation\n\nINSTAGRAM:\nhttps://instagram.com/jaiden_animations\n\nIf you're an animator, artist, gamer, or entertainer, then join us at Channel Frederator Network: https://frdr.us/Jaiden\n\nWanna Send Fanart?\nYou can send it to me through Twitter or Instagram (links above)\n------------------------------------------------------------------------------------\nMusic:\nHeartwarming (PMD2)\nSpinda's Shop (PMD2)\nKecleon's Shop (PMD)\nFriend Arena Steppe (PMD)",
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/qF1DTK4U1AM/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/qF1DTK4U1AM/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/qF1DTK4U1AM/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: 'https://i.ytimg.com/vi/qF1DTK4U1AM/sddefault.jpg',
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: 'https://i.ytimg.com/vi/qF1DTK4U1AM/maxresdefault.jpg',
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: 'Jaiden Animations',
+        categoryId: '1',
+        liveBroadcastContent: 'none',
+        defaultAudioLanguage: 'en',
+      },
+      contentDetails: {
+        duration: 'PT15M22S',
+        definition: 'hd',
+        caption: 'false',
+      },
+      statistics: {
+        viewCount: '4547673',
+        likeCount: '687572',
+        commentCount: '97703',
+      },
+    },
+    {
+      id: 'Lm60wE7P0js',
+      snippet: {
+        publishedAt: '2022-03-20T22:15:39Z',
+        channelId: 'UC6c1z7bA__85CIWZ_jpCK-Q',
+        defaultAudioLanguage: 'en-us',
+        title:
+          'Barca thrash Real Madrid in 4-0 El Clasico win | LaLiga Highlights | ESPN FC',
+        description:
+          'Pierre-Emerick Aubameyang scores twice and notches an assist in a comprehensive El Clasico win for Barcelona against Real Madrid.',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/Lm60wE7P0js/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/Lm60wE7P0js/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/Lm60wE7P0js/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: 'https://i.ytimg.com/vi/Lm60wE7P0js/sddefault.jpg',
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: 'https://i.ytimg.com/vi/Lm60wE7P0js/maxresdefault.jpg',
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: 'ESPN FC',
+        categoryId: '17',
+        liveBroadcastContent: 'none',
+      },
+      contentDetails: {
+        duration: 'PT10M26S',
+        definition: 'hd',
+        caption: 'true',
+      },
+      statistics: {
+        viewCount: '1032214',
+        likeCount: '16651',
+        commentCount: '2114',
+      },
+    },
+    {
+      id: 'jjXZJZ4F8Ak',
+      snippet: {
+        publishedAt: '2022-03-21T16:27:15Z',
+        channelId: 'UCWkYXtnAuu7VTLPwUcRSB6A',
+        title: 'To Our Son',
+        description:
+          'Director Tyler Ross @whitetrashtyler\nEditor Dylan Coughran @dylan_swg\nMusic Jacob Wilkinson-Smith @mybestfriendjacob_',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/jjXZJZ4F8Ak/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/jjXZJZ4F8Ak/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/jjXZJZ4F8Ak/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: 'https://i.ytimg.com/vi/jjXZJZ4F8Ak/sddefault.jpg',
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: 'https://i.ytimg.com/vi/jjXZJZ4F8Ak/maxresdefault.jpg',
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: 'Kylie Jenner',
+        categoryId: '22',
+        liveBroadcastContent: 'none',
+        defaultAudioLanguage: 'en',
+      },
+      contentDetails: {
+        duration: 'PT9M43S',
+        definition: 'hd',
+        caption: 'false',
+      },
+      statistics: {
+        viewCount: '1530444',
+        likeCount: '0',
+        commentCount: '9762',
+      },
+    },
+    {
+      id: 'LnlKwzc_TNA',
+      snippet: {
+        publishedAt: '2022-03-19T20:02:32Z',
+        channelId: 'UCX6OQ3DkcsbYNE6H8uQQuVA',
+        title: 'Would You Swim With Sharks For $100,000?',
+        description:
+          'Try Experian Boost to boost your credit scores: https://smart.link/n3op1gefxlzjn\n\n\nResults may vary. See App Store or website for details.\n\nNew Merch - https://shopmrbeast.com/\n\nSUBSCRIBE OR I TAKE YOUR DOG\n╔═╦╗╔╦╗╔═╦═╦╦╦╦╗╔═╗\n║╚╣║║║╚╣╚╣╔╣╔╣║╚╣═╣ \n╠╗║╚╝║║╠╗║╚╣║║║║║═╣\n╚═╩══╩═╩═╩═╩╝╚╩═╩═╝\n\n----------------------------------------------------------------\nfollow all of these or i will kick you\n• Facebook - https://www.facebook.com/MrBeast6000/\n• Twitter - https://twitter.com/MrBeast\n•  Instagram - https://www.instagram.com/mrbeast\n•  Im Hiring! - https://www.mrbeastjobs.com/\n--------------------------------------------------------------------',
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/LnlKwzc_TNA/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/LnlKwzc_TNA/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/LnlKwzc_TNA/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: 'https://i.ytimg.com/vi/LnlKwzc_TNA/sddefault.jpg',
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: 'https://i.ytimg.com/vi/LnlKwzc_TNA/maxresdefault.jpg',
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: 'MrBeast',
+        categoryId: '24',
+        liveBroadcastContent: 'none',
+        defaultAudioLanguage: 'en-US',
+      },
+      contentDetails: {
+        duration: 'PT9M',
+        definition: 'hd',
+        caption: 'false',
+      },
+      statistics: {
+        viewCount: '17066689',
+        likeCount: '1194562',
+        commentCount: '52447',
+      },
+    },
+    {
+      id: 'R9At2ICm4LQ',
+      snippet: {
+        publishedAt: '2022-03-21T09:07:08Z',
+        channelId: 'UCEf_Bc-KVd7onSeifS3py9g',
+        title: "Red Velvet 레드벨벳 'Feel My Rhythm' MV",
+        description:
+          "Red Velvet's mini album \"'The ReVe Festival 2022 - Feel My Rhythm'\" is out!\nListen and download on your favorite platform: https://smarturl.it/RV_FeelMyRhythm\n\nRed Velvet Official\nhttp://www.youtube.com/redvelvet\nhttp://www.facebook.com/RedVelvet\nhttp://www.instagram.com/redvelvet.smtown\nhttps://www.tiktok.com/@redvelvet_smtown\nhttp://twitter.com/RVsmtown\n\n#RedVelvet #Feel_My_Rhythm #The_ReVe_Festival_2022\n#레드벨벳 #필마이리듬\nRed Velvet 레드벨벳 'Feel My Rhythm' MV ℗ SM Entertainment",
+        thumbnails: {
+          default: {
+            url: 'https://i.ytimg.com/vi/R9At2ICm4LQ/default.jpg',
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: 'https://i.ytimg.com/vi/R9At2ICm4LQ/mqdefault.jpg',
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: 'https://i.ytimg.com/vi/R9At2ICm4LQ/hqdefault.jpg',
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: 'SMTOWN',
+        categoryId: '10',
+        liveBroadcastContent: 'none',
+        defaultAudioLanguage: 'ko',
+      },
+      contentDetails: {
+        duration: 'PT3M58S',
+        definition: 'hd',
+        caption: 'true',
+      },
+      statistics: {
+        viewCount: '5371203',
+        likeCount: '936567',
+        commentCount: '61189',
+      },
+    },
+  ],
+  nextPageToken: 'CAUQAA',
+  pageInfo: {
+    totalResults: 200,
+    resultsPerPage: 5,
+  },
+};
