9bf15f5393db3f290e07fe91c1e1990462750f81
Victor Oliveira <eu@victoroliveira.com.br>
2022-03-22 12:40:10 -0300

    feature(videos): VideosGrid refactored

--- src/features/videos/Grid/index.tsx
+++ src/features/videos/Grid/index.tsx
@@ -2,27 +2,46 @@ import { useMemo } from 'react';
 import CardGrid from 'components/CardGrid';
 import type { YoutubeVideo } from 'api/videos';
 import type { ICard } from 'components/Card';
+import {
+  formatViewCount,
+  getBestResolutionThumbUrl,
+} from 'shared/helpers/videos';
 
 interface IVideosGrid {
   videos: YoutubeVideo[];
 }
 
+function buildVideoCardSubtitle(video: YoutubeVideo) {
+  const stringBuilder = [video.snippet.channelTitle];
+  if (video.statistics)
+    stringBuilder.push(`${formatViewCount(video.statistics.viewCount)} views`);
+  return stringBuilder.join(' • ');
+}
+
+function mapper(video: YoutubeVideo) {
+  const {
+    snippet: { thumbnails, title },
+  } = video;
+  const id = typeof video.id === 'string' ? video.id : video.id.videoId;
+  const img = getBestResolutionThumbUrl(thumbnails);
+  const subtitle = buildVideoCardSubtitle(video);
+  const type = 'video';
+  const url = `/videos/${id}`;
+  return {
+    data: {
+      avatar: '',
+      id,
+      img,
+      subtitle,
+      title,
+      type,
+      url,
+    },
+  };
+}
+
 function VideosGrid({ videos }: IVideosGrid) {
-  const formattedVideos: ICard[] = useMemo(
-    () =>
-      videos.map((video) => ({
-        data: {
-          avatar: '',
-          id: typeof video.id === 'string' ? video.id : video.id.videoId,
-          img: '',
-          subtitle: '',
-          title: '',
-          type: '',
-          url: '',
-        },
-      })),
-    [videos]
-  );
+  const formattedVideos: ICard[] = useMemo(() => videos.map(mapper), [videos]);
 
   return <CardGrid data={formattedVideos} />;
 }
