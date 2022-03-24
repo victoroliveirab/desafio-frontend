5a32c2ca2cecd5bae1881899f81077796c7e355c
Victor Oliveira <eu@victoroliveira.com.br>
2022-03-24 15:06:06 -0300

    unused components removed

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,3 @@ export { default as GoogleLogin } from './GoogleLogin';
 export { default as Header } from './Header';
 export { default as InfiniteScroll } from './InfiniteScroll';
 export { default as SearchField } from './SearchField';
-export { default as VideoCard } from './VideoCard';
-export { default as VideosList } from './VideosList';
