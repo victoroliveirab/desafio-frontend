f102c518434c3b316c23e4ba0cc745d8c5aedbe3
Victor Oliveira <eu@victoroliveira.com.br>
2022-03-24 22:50:50 -0300

    feat(ux): feedback when failed login

--- src/components/GoogleLogin/index.tsx
+++ src/components/GoogleLogin/index.tsx
@@ -1,7 +1,7 @@
 import Button from '@mui/material/Button';
 import api, { openIdServices } from 'api';
 import { persistItem } from 'shared/helpers/local-storage';
-import { useAuth, useGoogleAuth } from 'shared/hooks';
+import { useAuth, useGoogleAuth, useUx } from 'shared/hooks';
 import type { IAuthUser } from 'shared/providers/auth/types';
 import { getOneHourLaterDate } from 'shared/helpers/datetime';
 import { scopes } from './constants';
@@ -15,6 +15,9 @@ function GoogleLogin() {
     actions: { setUser },
   } = useAuth();
   const { accounts } = useGoogleAuth();
+  const {
+    actions: { setAlert },
+  } = useUx();
 
   const requestAccessToken = () => {
     if (accounts?.oauth2) {
@@ -25,19 +28,27 @@ function GoogleLogin() {
         callback: (response: OpenIdResponse) => {
           const { access_token: accessToken } = response;
           api.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-          openIdServices.getProfileInfo().then(({ data }) => {
-            const userData: IAuthUser = {
-              email: data.email,
-              givenName: data.given_name,
-              picture: data.picture,
-            };
-            setUser(userData);
-            persistItem('google-user', {
-              user: userData,
-              token: accessToken,
-              expiration: date.toISOString(),
+          openIdServices
+            .getProfileInfo()
+            .then(({ data }) => {
+              const userData: IAuthUser = {
+                email: data.email,
+                givenName: data.given_name,
+                picture: data.picture,
+              };
+              setUser(userData);
+              persistItem('google-user', {
+                user: userData,
+                token: accessToken,
+                expiration: date.toISOString(),
+              });
+            })
+            .catch(() => {
+              setAlert({
+                message: 'Error while logging in. Please try again later.',
+                type: 'error',
+              });
             });
-          });
         },
       });
       client.requestAccessToken();
