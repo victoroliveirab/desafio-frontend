d8df62561c463cdf3511e396e90321ecdffa0757
Victor Oliveira <eu@victoroliveira.com.br>
2022-03-25 14:21:07 -0300

    feat(channel): not found channel page

--- src/pages/channel/index.tsx
+++ src/pages/channel/index.tsx
@@ -1,15 +1,31 @@
 import { useParams } from 'react-router-dom';
+import Link from '@mui/material/Link';
 import { videosServices } from 'api';
 import { VideosGrid } from 'features/videos';
 import InfiniteScrollYoutubeProvider from 'shared/providers/infinite-scroll-youtube';
 import type { YoutubeVideo } from 'api/videos';
 import { useInfiniteScrollGrid } from 'shared/hooks';
 
+function ChannelNotFound() {
+  return (
+    <div className="text-center">
+      <p>Channel not found</p>
+      <Link href="/channels">&larr; Back to channels</Link>
+    </div>
+  );
+}
+
 function ChannelPage() {
   const {
     state: { data },
   } = useInfiniteScrollGrid();
-  const videos = data as YoutubeVideo[];
+  const videos = Array.isArray(data) ? (data as YoutubeVideo[]) : undefined;
+
+  if (!videos) return <></>;
+
+  if (videos.length === 0) {
+    return <ChannelNotFound />;
+  }
 
   return (
     <InfiniteScrollYoutubeProvider>
@@ -23,6 +39,7 @@ function ChannelPageWrapper() {
   if (!channelId) return <></>;
   return (
     <InfiniteScrollYoutubeProvider
+      noStartEmpty
       service={() => videosServices.getByChannelId(channelId)}
     >
       <ChannelPage />
