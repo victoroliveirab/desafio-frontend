eb9a1208451a341ce0572f199424359d61a0e6cd
Victor Oliveira <eu@victoroliveira.com.br>
2022-03-25 01:58:59 -0300

    fix(uploadpage): rollback

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,45 +1,5 @@
-import { ChangeEvent, useState } from 'react';
-import api from 'api';
-
 function UploadPage() {
-  const [file, setFile] = useState<File | null>(null);
-  const handleFile = (event: ChangeEvent) => {
-    const { files } = event.target as HTMLInputElement;
-    if (!files || files.length === 0) return;
-    setFile(files[0]);
-  };
-  const send = () => {
-    if (!file) return;
-    const body = {
-      part: 'snippet,status',
-      snippet: {
-        categoryId: '22',
-        description: 'Description of uploaded video.',
-        title: 'Test video upload.',
-      },
-      status: {
-        privacyStatus: 'private',
-      },
-    };
-    const formData = new FormData();
-    formData.append('file', file, 'video_sample.mp4');
-    api
-      .post('https://www.googleapis.com/upload/youtube/v3/videos', formData)
-      .then(({ data }) => {
-        console.log(data);
-      });
-  };
-  return (
-    <>
-      Upload
-      <input type="file" name="file" onChange={handleFile} />
-      {file && (
-        <button type="button" onClick={send}>
-          Send
-        </button>
-      )}
-    </>
-  );
+  return <>Upload</>;
 }
 
 export default UploadPage;
