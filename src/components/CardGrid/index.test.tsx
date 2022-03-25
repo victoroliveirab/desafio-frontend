5cee0c49f740e5d326e74e947341fee9d71c7ae1
Victor Oliveira <eu@victoroliveira.com.br>
2022-03-25 00:02:25 -0300

    feat(card-grid): tests

--- src/components/CardGrid/index.test.tsx
+++ src/components/CardGrid/index.test.tsx
@@ -1,9 +1,15 @@
+import { MemoryRouter } from 'react-router-dom';
 import { render } from 'test-utils';
+import cardInfo from '__mocks__/card';
 import CardGrid from '.';
 
 describe('<CardGrid />', () => {
   test('Should match snapshot', () => {
-    const component = render(<CardGrid data={[]} />);
+    const component = render(
+      <MemoryRouter>
+        <CardGrid data={[cardInfo]} />
+      </MemoryRouter>
+    );
     expect(component).toMatchSnapshot();
   });
 });
